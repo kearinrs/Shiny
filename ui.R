3dafc7f86f7d6fa62648f8b67cca3bd0dc4cd13c
kearinrs <75562460+kearinrs@users.noreply.github.com>
2021-01-09 22:49:04 +0000

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,40 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+
+#setwd("/home/rsk17/R/x86_64-pc-linux-gnu-library/3.6/")
+
+
+dataset <- read.csv('2020_movies.csv')
+
+fluidPage(
+  titlePanel("2020_movies"),
+  
+  sidebarPanel(
+    
+    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                value=min(1000, nrow(dataset)), step=500, round=0),
+    
+    selectInput('x', 'X', names(dataset)),
+    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+    selectInput('color', 'Color', c('None', names(dataset))),
+    
+    checkboxInput('jitter', 'Jitter'),
+    checkboxInput('smooth', 'Smooth'),
+    
+    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+  ),
+  
+  mainPanel(
+    plotOutput('plot')
+  )
+  )
